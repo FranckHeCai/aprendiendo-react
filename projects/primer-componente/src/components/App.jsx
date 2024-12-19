5a3489813506993f8e384307156c7fdcbef44280
FranckHeCai <senlouboi.egame@gmail.com>
2024-12-19 18:41:59 +0100

    learning react

---
+++ projects/primer-componente/src/components/App.jsx
@@ -0,0 +1,18 @@
+export function App() {
+    return(
+        <article style={{"display": "flex"}}>
+            <header>
+                <img src="https://unavatar.io/midudev" alt="midudev avatar" />
+                <div>
+                    <strong>Midudev</strong>
+                    <span>@midudev</span>
+                </div>
+            </header>
+            <aside>
+                <button>
+                    Follow
+                </button>
+            </aside>
+        </article>
+    );
+}
