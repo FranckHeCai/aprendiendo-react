a58b206cb2b7fa8bbc8d8e383ed650ce33ed22fc
Franck Cai <senlouboi.egame@gmail.com>
2025-03-04 19:14:14 +0100

    valorant protocol

---
+++ projects/valorant-protocol/src/components/Header.jsx
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <div className="p-2 bg-neutral-800 text-red-400 font-[VALORANT] border-b-2  w-full fixed top-0 left-0 flex justify-between items-center">
+      <img src="./valorant-icon.svg" alt="valorant logo" />
+      <h2 className=" text-3xl" >Valorant Protocol</h2>
+      <p>more</p>
+    </div>
+  );
+};
+
+export default Header;
